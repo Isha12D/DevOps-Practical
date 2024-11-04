46eb68fb5c0a1fe1e95e4ff067050d71bc727675
Isha12D <ishadoifode6@gmail.com>
2024-11-04 14:20:10 +0530

    created a file first.cpp

---
+++ first.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout<<"DevOps Practical"<<endl;
+    int a = 8;
+    int b = 9;
+    cout<<"first sum is: "<<a+b<<endl;
+    return 0;
+}
